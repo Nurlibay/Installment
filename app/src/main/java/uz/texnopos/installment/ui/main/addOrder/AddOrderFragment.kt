5be5949afd6c4eb2476e10c3c04c35116cc4445b
Alisher154 <alisherqazaqbaev@gmail.com>
2021-12-06 23:41:56 +0500

    add order prepared

--- app/src/main/java/uz/texnopos/installment/ui/main/addOrder/AddOrderFragment.kt
+++ app/src/main/java/uz/texnopos/installment/ui/main/addOrder/AddOrderFragment.kt
@@ -4,77 +4,67 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import com.google.android.gms.common.util.CollectionUtils.listOf
-import okhttp3.internal.immutableListOf
+import androidx.lifecycle.MutableLiveData
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import uz.texnopos.installment.R
 import uz.texnopos.installment.core.*
+import uz.texnopos.installment.core.mask.MaskWatcherPrice
 import uz.texnopos.installment.data.model.PostOrder
 import uz.texnopos.installment.data.model.Product
-import uz.texnopos.installment.databinding.AddOrderDialogBinding
+import uz.texnopos.installment.databinding.FragmentAddOrderBinding
 
-class AddOrderFragment : Fragment(R.layout.add_order_dialog) {
+class AddOrderFragment : Fragment(R.layout.fragment_add_order) {
 
-    private lateinit var binding: AddOrderDialogBinding
+    private lateinit var binding: FragmentAddOrderBinding
     private val viewModel: AddOrderViewModel by viewModel()
     private var clientId: Int = 0
-    private var productList: List<Product> = listOf()
-    private var productNames: MutableList<String> = mutableListOf()
     private var productId: Int = 0
+    private val products = MutableLiveData<List<Product>?>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         clientId = arguments?.getInt("client_id")!!
     }
 
+    override fun onStart() {
+        super.onStart()
+        viewModel.getProducts()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = AddOrderDialogBinding.bind(view)
-        viewModel.getProducts()
-        viewModel.getProducts.observe(requireActivity(), {
-            when (it.status) {
-                ResourceState.LOADING -> showProgress()
-                ResourceState.SUCCESS -> {
-                    hideProgress()
-                    productList = it.data!!.payload
-                    productList.forEach {
-                        productNames.add(it.product_name)
-                    }
-                }
-                ResourceState.ERROR -> {
-                    hideProgress()
-                    toast(it.message!!)
-                }
-                ResourceState.NETWORK_ERROR -> {
-                    hideProgress()
-                    toast(Constants.NO_INTERNET)
-                }
-            }
-        })
-
-        val arrayAdapter = ArrayAdapter(requireContext(),
-            R.layout.item_drop_down,
-            productList.map { it.product_name })
-        binding.productName.setAdapter(arrayAdapter)
-        binding.productName.setOnItemClickListener { parent, view, position, id ->
-            productId = productList[position].product_id
-            toast(productId.toString())
-            toast(binding.productName.toString())
-        }
+        binding = FragmentAddOrderBinding.bind(view)
         setUpObserverOrder()
+        observeProducts()
         binding.apply {
             toolbar.navOnClick {
                 requireActivity().onBackPressed()
             }
+
+            etFirstPay.addTextChangedListener(MaskWatcherPrice(etFirstPay))
+            etPrice.addTextChangedListener(MaskWatcherPrice(etPrice))
+            products.observe(requireActivity(), {
+                if (it != null) {
+                    val arrayAdapter = ArrayAdapter(requireContext(), R.layout.item_drop_down,
+                        it.map { p ->
+                            p.product_name
+                        })
+                    productName.setAdapter(arrayAdapter)
+                    productName.setOnItemClickListener { _, _, position, _ ->
+                        productId = it[position].product_id
+                    }
+                }
+            })
+
             btnAddOrder.onClick {
                 if (validate()) {
                     val newOrder = PostOrder(
                         product_id = productId.toString(),
                         client_id = clientId.toString(),
-                        first_pay = etFirstPay.textToString(),
+                        first_pay = etFirstPay.textToString().getOnlyDigits(),
                         month = etMonth.textToString(),
                         surcharge = etSurcharge.textToString(),
-                        price = etPrice.textToString(),
+                        price = etPrice.textToString().getOnlyDigits(),
                         product_code = etProductCode.textToString()
                     )
                     viewModel.addOrder(newOrder)
@@ -104,7 +94,24 @@ class AddOrderFragment : Fragment(R.layout.add_order_dialog) {
         })
     }
 
-    private fun AddOrderDialogBinding.validate(): Boolean {
+    private fun observeProducts() {
+        viewModel.getProducts.observe(requireActivity(), {
+            when (it.status) {
+                ResourceState.LOADING -> {}
+                ResourceState.SUCCESS -> {
+                    if (it.data != null) products.postValue(it.data.payload)
+                }
+                ResourceState.ERROR -> {
+                    toast(it.message!!)
+                }
+                ResourceState.NETWORK_ERROR -> {
+                    toast(Constants.NO_INTERNET)
+                }
+            }
+        })
+    }
+
+    private fun FragmentAddOrderBinding.validate(): Boolean {
         return when {
             etFirstPay.checkIsEmpty() -> etFirstPay.showError(getString(R.string.required))
             etMonth.checkIsEmpty() -> etMonth.showError(getString(R.string.required))
